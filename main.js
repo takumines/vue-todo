545bfe6786ebcc59bf3992bcb55ef0e2ce1cbacf
takumi_kurogi <takumines1@gmail.com>
2020-11-21 18:43:14 +0900

    ストレージへの保存の自動化

---
+++ main.js
@@ -0,0 +1,53 @@
+// https://jp.vuejs.org/v2/examples/todomvc.html
+var STORAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = {
+    fetch: function () {
+        var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+        todos.forEach(function (todo, index) {
+            todo.id = index
+        })
+        todoStorage.uid = todos.length
+        return todos
+    },
+    save: function (todos) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+    }
+}
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        todos: []
+    },
+    methods: {
+        // ToDo 追加の処理
+        doAdd: function(event, value) {
+            // ref で名前を付けておいた要素を参照
+            var comment = this.$refs.comment
+            // 入力がなければ何もしないで return
+            if (!comment.value.length) {
+                return
+            }
+            // { 新しいID, コメント, 作業状態 }
+            // というオブジェクトを現在の todos リストへ push
+            // 作業状態「state」はデフォルト「作業中=0」で作成
+            this.todos.push({
+                id: todoStorage.uid++,
+                comment: comment.value,
+                state: 0
+            })
+            // フォーム要素を空にする
+            comment.value = ''
+        }
+    },
+    watch: {
+        // オプションを使う場合はオブジェクト型にする
+        todos: {
+            // 引数はウォッチしているプロパティの変更後の値
+            handler: function (todos) {
+                todoStorage.save(todos)
+            },
+            deep: true
+        }
+    }
+})
